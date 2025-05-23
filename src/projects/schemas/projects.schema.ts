ffc0bee2e50594d2ee01567623f17b7962f6904c
Ibañes Franklin Perez Muñoz <ibanesp59@gmail.com>
2025-05-23 06:03:08 -0500

    fix: update import paths to use relative paths

--- src/projects/schemas/projects.schema.ts
+++ src/projects/schemas/projects.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
-import { User } from 'users/schemas/users.schema';
+import { User } from '../../users/schemas/users.schema';
 
 export type ProjectDocument = Project & Document;
 
